916aaa209ffb65d5824c86ee391ccccc43ecb988
Akash <einsteinakash007@gmail.com>
2023-09-17 22:08:05 +0530

    data collection pipeline with ganglion

--- read_write.py
+++ read_write.py
@@ -6,7 +6,7 @@ from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, Boa
 from brainflow.data_filter import DataFilter
 
 
-def main():
+def main(t, folder_name):
     BoardShim.enable_dev_board_logger()
 
     # use synthetic board for demo
@@ -15,7 +15,7 @@ def main():
     board.prepare_session()
     board.start_stream()
     BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
-    time.sleep(10)
+    time.sleep(t)
     data = board.get_board_data()
     board.stop_stream()
     board.release_session()
@@ -27,12 +27,16 @@ def main():
     print(df.head(10))
 
     # demo for data serialization using brainflow API, we recommend to use it instead pandas.to_csv()
-    DataFilter.write_file(data, 'test.csv', 'w')  # use 'a' for append mode
-    restored_data = DataFilter.read_file('test.csv')
+    DataFilter.write_file(data, f'{folder_name}/test_{t}_{int(time.time())}.csv', 'w')  # use 'a' for append mode
+    restored_data = DataFilter.read_file(f'{folder_name}/test_{t}_{int(time.time())}.csv')
     restored_df = pd.DataFrame(np.transpose(restored_data))
     print('Data From the File')
     print(restored_df.head(10))
 
 
 if __name__ == "__main__":
-    main()
+    n_sample = 5
+    n_sec = 60
+    folder_name = 'think_data'
+    for i in range(n_sample):
+        main(n_sec, folder_name)
